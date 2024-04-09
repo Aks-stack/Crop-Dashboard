154d50c195d369e9fd914ca56469c20659c2c98a
Sayan Kar <karsayan599@gmail.com>
2024-04-09 18:26:56 +0530

    rightbar-colored

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import RightBar from "./components/RightBar";
-import SideB from "./components/SideB";
 
 function App() {
   return (
     <div className="App">
-      <SideB />
       <RightBar />
     </div>
   );
