535258026e6716844cbcdd3371aac05b63b7f005
Sayan Kar <karsayan599@gmail.com>
2024-04-10 01:37:44 +0530

    prediction-box-added

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Chart } from "react-google-charts";
 import "./rightbar.css";
 import buttonChart from "../assets/Button.svg";
+import plantPredict from "../assets/plant_7963920.png";
+import tempPredict from "../assets/sun_2698213.png";
 
 function RightBar() {
     const data1 = [
@@ -21,69 +23,89 @@ function RightBar() {
     return (
         <div className='right-container'>
             <div className='right-container-title'>Main Dashboard</div>
+            <div className='bottom'>
 
-            <div className='right-container-col1'>
+                {/* Taking 60% of the Right-part */}
+                <div className='right-container-col1'>
 
-                {/* Chart - 1 */}
-                <div className='chart-container'>
-                    <div className='chart-container-top'>
-                        <p>Soil Temperature & Moisture</p>
-                        <img src={buttonChart} alt="" />
+                    {/* Chart - 1 */}
+                    <div className='chart-container'>
+                        <div className='chart-container-top'>
+                            <p>Soil Temperature & Moisture</p>
+                            <img src={buttonChart} alt="" />
+                        </div>
+                        <Chart
+                            className='chart'
+                            chartType="LineChart"
+                            style={{ width: "100%" }}
+                            data={data1}
+                            options={options}
+                        />
                     </div>
-                    <Chart
-                        className='chart'
-                        chartType="LineChart"
-                        style={{ width: "100%" }}
-                        data={data1}
-                        options={options}
-                    />
+
+                    {/* Chart - 2 */}
+                    <div className='chart-container'>
+                        <div className='chart-container-top'>
+                            <p>N P K Data</p>
+                            <img src={buttonChart} alt="" />
+                        </div>
+                        <Chart
+                            className='chart'
+                            chartType="LineChart"
+                            style={{ width: "100%" }}
+                            data={data1}
+                            options={options}
+                        />
+                    </div>
+
+                    {/* Chart - 3 */}
+                    <div className='chart-container'>
+                        <div className='chart-container-top'>
+                            <p>Temperature & Humidity</p>
+                            <img src={buttonChart} alt="" />
+                        </div>
+                        <Chart
+                            className='chart'
+                            chartType="LineChart"
+                            style={{ width: "100%" }}
+                            data={data1}
+                            options={options}
+                        />
+                    </div>
+
+                    {/* Chart - 4 */}
+                    <div className='chart-container'>
+                        <div className='chart-container-top'>
+                            <p>PH Value</p>
+                            <img src={buttonChart} alt="" />
+                        </div>
+                        <Chart
+                            className='chart'
+                            chartType="LineChart"
+                            style={{ width: "100%" }}
+                            data={data1}
+                            options={options}
+                        />
+                    </div>
                 </div>
 
-                {/* Chart - 2 */}
-                <div className='chart-container'>
-                    <div className='chart-container-top'>
-                        <p>N P K Data</p>
-                        <img src={buttonChart} alt="" />
+                {/* Taking 40% of the Right-Bar */}
+                <div className='right-container-col2'>
+                    <div className='crop-recommend container'>
+                        <div className='chart-container-top'>
+                            <p>Crop Recommendation</p>
+                            <img src={plantPredict} height={25} width={25} alt="" />
+                        </div>
                     </div>
-                    <Chart
-                        className='chart'
-                        chartType="LineChart"
-                        style={{ width: "100%" }}
-                        data={data1}
-                        options={options}
-                    />
-                </div>
-
-                {/* Chart - 3 */}
-                <div className='chart-container'>
-                    <div className='chart-container-top'>
-                        <p>Temperature & Humidity</p>
-                        <img src={buttonChart} alt="" />
+                    <div className='temperature container'>
+                        <div className='chart-container-top'>
+                            <p>Temperature Prediction</p>
+                            <img src={tempPredict} height={25} width={25} alt="" />
+                        </div>
                     </div>
-                    <Chart
-                        className='chart'
-                        chartType="LineChart"
-                        style={{ width: "100%" }}
-                        data={data1}
-                        options={options}
-                    />
-                </div>
-
-                {/* Chart - 4 */}
-                <div className='chart-container'>
-                    <div className='chart-container-top'>
-                        <p>PH Value</p>
-                        <img src={buttonChart} alt="" />
-                    </div>
-                    <Chart
-                        className='chart'
-                        chartType="LineChart"
-                        style={{ width: "100%" }}
-                        data={data1}
-                        options={options}
-                    />
                 </div>
             </div>
+
         </div>
     )
 }
