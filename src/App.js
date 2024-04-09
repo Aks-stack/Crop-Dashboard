9abfb32849bd9202558be62ed50724dfad5de162
Sayan Kar <karsayan599@gmail.com>
2024-04-09 20:59:45 +0530

    added-charts-main-component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <SideB />
-      Hello World
       <RightBar />
     </div>
   );
--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,11 +1,79 @@
 import React from 'react'
+import { Chart } from "react-google-charts";
 import "./rightbar.css";
+import buttonChart from "../assets/Button.svg";
 
 function RightBar() {
+    const data1 = [
+        ["Year", "Temperature", "Moisture"],
+        ["2004", 1000, 400],
+        ["2005", 1170, 460],
+        ["2006", 660, 1120],
+        ["2007", 1030, 540],
+    ];
+    const options = {
+        width: "450px",
+        curveType: "function",
+        legend: { position: "bottom" },
+    };
     return (
         <div className='right-container'>
             <div className='right-container-title'>Main Dashboard</div>
-            <div className='right-container-col1'></div>
+            <div className='right-container-col1'>
+                <div className='chart-container'>
+                    <div className='chart-container-top'>
+                        <p>Soil Temperature & Moisture</p>
+                        <img src={buttonChart} alt="" />
+                    </div>
+                    <Chart
+                        className='chart'
+                        chartType="LineChart"
+                        style={{ width: "100%" }}
+                        data={data1}
+                        options={options}
+                    />
+                </div>
+                <div className='chart-container'>
+                    <div className='chart-container-top'>
+                        <p>N P K Data</p>
+                        <img src={buttonChart} alt="" />
+                    </div>
+                    <Chart
+                        className='chart'
+                        chartType="LineChart"
+                        style={{ width: "100%" }}
+                        data={data1}
+                        options={options}
+                    />
+                </div>
+                <div className='chart-container'>
+                    <div className='chart-container-top'>
+                        <p>Temperature & Humidity</p>
+                        <img src={buttonChart} alt="" />
+                    </div>
+                    <Chart
+                        className='chart'
+                        chartType="LineChart"
+                        style={{ width: "100%" }}
+                        data={data1}
+                        options={options}
+                    />
+                </div>
+                <div className='chart-container'>
+                    <div className='chart-container-top'>
+                        <p>PH Value</p>
+                        <img src={buttonChart} alt="" />
+                    </div>
+                    <Chart
+                        className='chart'
+                        chartType="LineChart"
+                        style={{ width: "100%" }}
+                        data={data1}
+                        options={options}
+                    />
+                </div>
+            </div>
+
         </div>
     )
 }
