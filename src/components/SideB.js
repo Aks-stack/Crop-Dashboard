fbd9963a3457f32de6e4ac708385d6b7d3ce8789
Sayan Kar <karsayan599@gmail.com>
2024-04-10 19:52:52 +0530

    added-routes

--- src/components/SideB.js
+++ src/components/SideB.js
@@ -6,6 +6,7 @@ import logo from '../assets/leaf_2917995.png'
 import IconInfo from '../assets/IconInfo'
 import profile from '../assets/profile_3135715.png'
 import IconWeatherNight from '../assets/IconWeatherNight'
+import { Link } from 'react-router-dom'
 
 export default function SideB(props) {
 
@@ -29,25 +30,30 @@ export default function SideB(props) {
                 <img src={logo} style={{ width: '40px', height: '40px' }} alt='/' />
             </div>
             <div className="routes">
-                <div className="icons">
-                    <a href='/' className='link'>
-                        <IconHome />
-                    </a>
-                </div>
-                {/* <div className="title">Home</div> */}
-                <div className="icons">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24px" fill="currentColor" height="24px">
-                        <path d="M128 0h64c17.7 0 32 14.3 32 32v96H96V32c0-17.7 14.3-32 32-32zM0 256c0-53 43-96 96-96H224c53 0 96 43 96 96V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V256zm240 80A80 80 0 1 0 80 336a80 80 0 1 0 160 0zM256 64a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM384 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm64 32a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM448 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM384 128a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
-                    </svg>
-                </div>
-                <div className="icons">
-                    <IconInfo />
-                </div>
-                {/* <div className="titile">about</div> */}
-                <div className="icons">
-                    <IconSettingsSharp />
-                </div>
-                {/* <div className="title">Settings</div> */}
+                <Link to="/">
+                    <div className="icons">
+                        <a href='/' className='link'>
+                            <IconHome />
+                        </a>
+                    </div>
+                </Link>
+                <Link to="/fertilizer">
+                    <div className="icons">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24px" fill="currentColor" height="24px">
+                            <path d="M128 0h64c17.7 0 32 14.3 32 32v96H96V32c0-17.7 14.3-32 32-32zM0 256c0-53 43-96 96-96H224c53 0 96 43 96 96V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V256zm240 80A80 80 0 1 0 80 336a80 80 0 1 0 160 0zM256 64a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM384 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm64 32a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM448 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM384 128a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+                        </svg>
+                    </div>
+                </Link>
+                <Link to="/about">
+                    <div className="icons">
+                        <IconInfo />
+                    </div>
+                </Link>
+                <Link to="/settings">
+                    <div className="icons">
+                        <IconSettingsSharp />
+                    </div>
+                </Link>
                 <div className="user">
                     <img src={profile} alt='/' style={{ width: '30px', height: '30px' }} />
                 </div>
